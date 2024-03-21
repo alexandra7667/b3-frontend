10c1cbf58eca7a739ecabcbac77371fc3484325b
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-03-21 15:40:11 +0100

    some adds to dashboard styling

--- bobs-bodybuilding-fe/src/Header/index.jsx
+++ bobs-bodybuilding-fe/src/Header/index.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import { useNavigate } from "react-router-dom";
 
-export default function Header({ loggedIn, setLoggedIn }) {
+export default function Header({ loggedIn, setLoggedIn, userName }) {
   const appTitle = "Bob's Bodybuilding"
 
   const navigate = useNavigate();
@@ -21,7 +21,10 @@ export default function Header({ loggedIn, setLoggedIn }) {
       <h1 className="title">{appTitle}</h1>
       {loggedIn && (
         <>
-          <button onClick={logout}>Log out</button>
+          <div className="user-creds">
+          <h3 className="user-name">{userName}</h3>
+            <button className="logout-btn" onClick={logout}>Log out</button>
+          </div>
         </>
       )}
     </header>
